f3dbe5abfa64d01ddeb172223a30b5077b1da0f6
abdenacer <nasseressaouira@gmail.com>
2023-02-22 20:34:48 +0800

    ..

--- Backend/src/utils/images/deleteFile.ts
+++ Backend/src/utils/images/deleteFile.ts
@@ -4,8 +4,8 @@ import { Request } from 'express'
 class deleteImage {
     public deleteFile = (imagePath: string) => {
         try {
-            fs.unlinkSync(imagePath)
-            console.log('Deleted image:', imagePath)
+            fs.unlinkSync('public/' + imagePath)
+            // console.log('Deleted image:', imagePath)
           } catch (err) {
             console.log('Error deleting image:', err)
           }
