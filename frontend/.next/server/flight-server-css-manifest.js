e8454a2b26ca5133538331adf685ce1997fae268
abdenacer <nasseressaouira@gmail.com>
2023-02-23 13:51:38 +0800

    renamed mobile project to mobile

---
+++ frontend/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\src\\app\\page": [
+      "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\src\\app\\page.module.css",
+      "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\page.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\src\\app\\globals.css"
+    ]
+  },
+  "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\src\\app\\page.js": [
+    "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\src\\app\\page.module.css",
+    "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\page.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\src\\app\\layout.js": [
+    "C:\\Users\\Youcode\\Desktop\\Marketify\\frontend\\src\\app\\globals.css"
+  ]
+}
