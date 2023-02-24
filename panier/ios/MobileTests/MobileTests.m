ab6835faa970e08f819aa2f77e8a009031aad6b0
FATIHA99 <fatihhaa27@gmail.com>
2023-02-24 19:05:49 +0100

    done

--- panier/ios/MobileTests/MobileTests.m
+++ panier/ios/MobileTests/MobileTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface panierTests : XCTestCase
+@interface MobileTests : XCTestCase
 
 @end
 
-@implementation panierTests
+@implementation MobileTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
